b2b2b1fea15f2b80fdab9c8393c141a30fdcb502
Eric Haibin Lin <linhaibin.eric@gmail.com>
2015-12-05 21:58:06 -0500

    + add common connection struct which may be used by Master node and Client node.

---
+++ cmu440-F15/paxosapp/common/common.go
@@ -0,0 +1,12 @@
+package common
+
+import (
+	"net/rpc"
+)
+
+// A Conn struct contains the hostPort of connection endpoint and the dialer 
+// to this hostport 
+type Conn struct {
+	HostPort string
+	Dialer *rpc.Client 
+}
