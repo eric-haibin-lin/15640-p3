dc355aa34d8f01ff2f618534ac63af79c74feb9b
Eric Haibin Lin <linhaibin.eric@gmail.com>
2015-12-06 19:53:44 -0500

    go fmt crunner.

--- cmu440-F15/paxosapp/runners/crunner/crunner.go
+++ cmu440-F15/paxosapp/runners/crunner/crunner.go
@@ -5,16 +5,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/cmu440-F15/paxosapp/client"
 	"log"
 	"strings"
-	"github.com/cmu440-F15/paxosapp/client"
 )
 
 var (
 	port       = flag.String("port", "", "port for client node")
-	opt       = flag.String("opt", "", "option for client node, either crawl or pagerank")
-	num       = flag.Int("num", 10, "number of webpages to crawl")
-	url       = flag.String("url", "http://news.google.com", "root url to start crawling with")
+	opt        = flag.String("opt", "", "option for client node, either crawl or pagerank")
+	num        = flag.Int("num", 10, "number of webpages to crawl")
+	url        = flag.String("url", "http://news.google.com", "root url to start crawling with")
 	masterPort = flag.String("masterPort", "", "port for master node")
 )
 
@@ -24,11 +24,11 @@ func init() {
 
 func main() {
 	flag.Parse()
-	
+
 	myHostPort := "localhost:" + *port
 	masterHostPort := strings.Split(*masterPort, ",")
 	fmt.Println("crunner creates client node on", myHostPort, "with master on ", *masterPort)
-	
+
 	// Create and start the Paxos Node.
 	cli, err := client.NewClientNode(myHostPort, masterHostPort)
 	if err != nil {
@@ -36,38 +36,38 @@ func main() {
 	}
 
 	var absoluteUrl string
-	if !strings.Contains(*url, "http://")  && !strings.Contains(*url, "https://") {
-		absoluteUrl = "http://" + *url 
+	if !strings.Contains(*url, "http://") && !strings.Contains(*url, "https://") {
+		absoluteUrl = "http://" + *url
 	}
-	
+
 	if *opt == "crawl" {
-		fmt.Println("crunner invokes Crawl on client" )
+		fmt.Println("crunner invokes Crawl on client")
 		var args client.CrawlArgs
 		var reply client.CrawlReply
 		args.RootUrl = absoluteUrl
 		args.NumPages = *num
-		cli.Crawl(&args, &reply) 	
-		
+		cli.Crawl(&args, &reply)
+
 	} else if *opt == "getlink" {
-		fmt.Println("crunner invokes GetLink on client" )
+		fmt.Println("crunner invokes GetLink on client")
 		var getLinkArgs client.GetLinksArgs
 		getLinkArgs.Url = absoluteUrl
 		var getLinkReply client.GetLinksReply
 		cli.GetLinks(&getLinkArgs, &getLinkReply)
-		
+
 	} else if *opt == "pagerank" {
-		fmt.Println("crunner invokes RunPageRank on client" )
+		fmt.Println("crunner invokes RunPageRank on client")
 		var pageRankArgs client.PageRankArgs
 		var pageRankReply client.PageRankReply
 		cli.RunPageRank(&pageRankArgs, &pageRankReply)
-		
+
 	} else if *opt == "getrank" {
-		fmt.Println("crunner invokes GetRank on client" )
-		fmt.Println("crunner invokes GetLink on client" )
+		fmt.Println("crunner invokes GetRank on client")
+		fmt.Println("crunner invokes GetLink on client")
 		var getRankArgs client.GetRankArgs
 		getRankArgs.Url = absoluteUrl
 		var getRankReply client.GetRankReply
 		cli.GetRank(&getRankArgs, &getRankReply)
-		
+
 	}
 }
